1cacd964b8d2cb3dc8734b7184c7216229d751ad
Shamsik <xz.2804@mail.ru>
2022-12-29 10:32:54 +0400

    first commit

---
+++ BL/GameBl.cs
@@ -0,0 +1,28 @@
+﻿using BL.Interfaces;
+using DAL.Interfaces;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL
+{
+    public class GameBl : IGameBl
+    {
+        private readonly IGameDal _dal;
+
+        public GameBl(IGameDal dal)
+        {
+            _dal = dal;
+        }
+        public Game GetbyUserId(int UserId)
+        {
+            return _dal.GetbyUserId(UserId);
+        }
+
+        public void PutGame(Game game)
+        {
+            _dal.PutGame(game);
+        }
+    }
+}
---
+++ BL/UsersBL.cs
@@ -0,0 +1,32 @@
+﻿using Entities;
+using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL
+{
+    public class UsersBL : IUsersBL
+    {
+        private IUsersDal _dal;
+
+        public UsersBL(IUsersDal dal)
+        {
+            _dal = dal;
+        }
+
+        public Users GetByLogin(string login) {
+            return _dal.GetByLogin(login);
+        }
+
+        public Users GetById(int id)
+        {
+            return _dal.GetById(id);
+        }
+
+        public void PutUser(Users user)
+        {
+            _dal.PutUser(user);
+        }
+    }
+}
